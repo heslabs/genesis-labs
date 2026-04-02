edf18104e9e185cf5739542a97ca49002a32259e
heslabs <soclabs@heswiki.com>
2026-04-02 13:22:38 +0800

    Create lab5.py

---
+++ hello/lab5.py
@@ -0,0 +1,66 @@
+import numpy as np
+import genesis as gs
+
+gs.init(backend=gs.cpu)
+
+scene = gs.Scene(
+    sim_options=gs.options.SimOptions(dt=0.01),
+    viewer_options=gs.options.ViewerOptions(
+        camera_pos=(3, -1, 1.5),
+        camera_lookat=(0.0, 0.0, 0.5),
+        camera_fov=30, max_FPS=60,
+    ),
+    show_viewer=True,
+)
+
+plane  = scene.add_entity(gs.morphs.Plane())
+cube   = scene.add_entity(gs.morphs.Box(
+    size=(0.04, 0.04, 0.04), pos=(0.65, 0.0, 0.02),
+))
+franka = scene.add_entity(gs.morphs.MJCF(file='xml/franka_emika_panda/panda.xml'))
+scene.build()
+
+motors_dof  = np.arange(7)
+fingers_dof = np.arange(7, 9)
+end_effector = franka.get_link('hand')
+
+# --- Move to pre-grasp pose via motion planning ---
+qpos = franka.inverse_kinematics(
+    link=end_effector,
+    pos=np.array([0.65, 0.0, 0.25]),
+    quat=np.array([0, 1, 0, 0]),
+)
+qpos[-2:] = 0.04  # open gripper
+
+path = franka.plan_path(qpos_goal=qpos, num_waypoints=200)
+for waypoint in path:
+    franka.control_dofs_position(waypoint)
+    scene.step()
+for _ in range(100):
+    scene.step()
+
+# --- Reach grasp pose ---
+qpos = franka.inverse_kinematics(
+    link=end_effector,
+    pos=np.array([0.65, 0.0, 0.130]),
+    quat=np.array([0, 1, 0, 0]),
+)
+franka.control_dofs_position(qpos[:-2], motors_dof)
+for _ in range(100):
+    scene.step()
+
+# --- Close gripper (force control) ---
+franka.control_dofs_force(np.array([-0.5, -0.5]), fingers_dof)
+for _ in range(100):
+    scene.step()
+
+# --- Lift ---
+qpos = franka.inverse_kinematics(
+    link=end_effector,
+    pos=np.array([0.65, 0.0, 0.28]),
+    quat=np.array([0, 1, 0, 0]),
+)
+franka.control_dofs_position(qpos[:-2], motors_dof)
+for _ in range(200):
+    scene.step()
+
